8004cc9f89d96084502654f5e371c7da463e8003
cuizheng <zheng.cuizh@gmail.com>
2011-07-08 14:11:29 +0800

    delete aliww & libsmtp--

--- features/aliww/build/debug/run.sh
+++
@@ -1,1 +0,0 @@
- ../../../../build/debug/cbenchmark -c 200 -T 36000 -F ./libaliww.so -l 1   
--- features/smtp/lib/libsmtp---0.1.0/doc/examples/make.sh
+++
@@ -1,1 +0,0 @@
-gcc -g -I /usr/local/libsmtp---0.1.0/include/ -lsmtp-- -L /usr/local/libsmtp---0.1.0/lib/ example_2.c -o example_2
